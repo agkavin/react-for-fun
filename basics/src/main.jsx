a207061b39ac97d63cef00278ac0d454aadb2525
agkavin <agkavin7@gmail.com>
2024-08-03 19:40:09 +0530

    added pages folder

--- basics/src/main.jsx
+++ basics/src/main.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Card from './Card.jsx'
+import {createBrowserRouter , RouterProvider} from 'react-router-dom'
+import App from './pages/App.jsx'
+import NotFoundPage from './pages/Error404Page.jsx'
+import Card from './pages/Card.jsx'
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <NotFoundPage />
+  },
+  {
+    path: "/card",
+    element: <Card />
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Card />
+    <RouterProvider  router={router}/>
   </React.StrictMode>,
-)
+);
---
+++ basics/src/pages/Error404Page.jsx
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom';
+
+export default function NotFoundPage() {
+  return (
+    <div>
+      404 Not Found
+      <br />
+      <Link to="/">Home from Link</Link>
+    </div>
+  );
+}
